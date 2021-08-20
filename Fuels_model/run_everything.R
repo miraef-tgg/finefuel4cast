adeea135bb8e35cfd417f466ff37cbf5169c2e4e
mensleyf <mensleyf@macalester.edu>
2021-08-20 14:40:33 -0600

    added gc() everywhere

--- Fuels_model/run_everything.R
+++ Fuels_model/run_everything.R
@@ -13,28 +13,35 @@ setwd(current_folder)
 
 # run models
 source(file = "fuels_model_run/fuels_model_run.R")
+gc()
 source(file = "fuels_model_run/fuels_model_run_ppc.R")
-# source(file = "fuels_model_run/fuels_model_prior_sensitivity_analysis.R") #takes a long time (days) to run
+gc()
+source(file = "fuels_model_run/fuels_model_prior_sensitivity_analysis.R") #takes a long time (days) to run
+gc()
 
 #model code
-file.show("stan_txts/fuels_model_code.txt")
+# file.show("stan_txts/fuels_model_code.txt")
 
-#-------model checking and visualization ----------- 
+#-------model checking and visualization -----------
+
 #our favorite way to check a model
-fit1<-readRDS( "model_outputs/fuels_model.rds")
-shinystan::launch_shinystan(fit1)
+# fit1<-readRDS( "model_outputs/fuels_model.rds")
+# shinystan::launch_shinystan(fit1)
 
 #traceplots and rhat scores
 source(file= "model_checking_and_validation/convergeance_check.R")
+gc()
 
 #ppc
 source(file= "model_checking_and_validation/predictive_posterior_plots.R")
+gc()
 
 #covariance between parameters
 source(file= "model_checking_and_validation/covariance_vis.R")
 
 #prior posterior plots
 source(file= "model_checking_and_validation/prior_posterior_plots.R")
+gc()
 
 # coverage:
 source(file="model_checking_and_validation/coverage_function.R")
@@ -44,24 +51,28 @@ uncertainty<-plot_coverage(fit1, q_upper=.975, q_lower=.025,
                            prod_start_val_vec<-meta_dat$P0,
                            prod_m_match<-meta_dat$prod_m_match,
                            mean_samp=4.6, sig_f_par=.8, show_plot=T)
-
+gc()
 
 #sensitivity analyses vis: this will take a long time (>30 min) to run
 # you can look at diagnostic plots, coverage plots, and changes in posteriors in "Supplemental_Info_figs/prior_sensitivity_analysis_figs" 
 # all plots are organized such that x axis is changes in one part of a prior
-source(file="model_checking_and_validation/sensitivity_analysis_vis.R")
+# source(file="model_checking_and_validation/sensitivity_analysis_vis.R")
+
+# parameter retrieval from simulated data
+# runs model (relatively fast) on simulated data 
+source(file = "fuels_model_run/sim_data_model_run.R")
+gc()
+#-------Supplemental Information / other considerations -----------
 
 
-
-# simulation
+# inital conditions simulation
 # simulates productivity data from actual productivity data
 # simulates latent fuels data from fuels model
 #looks at how sensitive latent fuel estimation is to initial value (not very)
 source(file="Supplemental_Info_figs/initial_conditions_spin_up.R")
+gc()
 
 # a quick look using linear regression at how parameter estimates of carryover (a), conversion (b),
 # as well as correlation, r squared, and mean absolute predictive error change by data transformation
 source(file="Supplemental_Info_figs/transformation_figs.R")
-
-
-# Supplemental information:
+gc()
