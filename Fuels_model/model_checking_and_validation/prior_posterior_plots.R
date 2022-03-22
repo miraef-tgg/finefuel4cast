71bdf542632f0ea5c9310a6026a3e97686f64065
mensleyf <miraensley@gmail.com>
2022-03-22 11:24:45 -0600

    updating the figures

--- Fuels_model/model_checking_and_validation/prior_posterior_plots.R
+++ Fuels_model/model_checking_and_validation/prior_posterior_plots.R
@@ -28,24 +28,28 @@ nIters<-nrow(sig_pout)
 par(mfrow=c(2,2))
 
 alpha_prior<-rnorm(nIters,.25,.25);
-plot(density(alphaout), xlim=c(-1,1), main="alpha", xlab="", cex.axis=1,cex.main=3,cex.lab=2)
+plot(density(alphaout), xlim=c(-1,1), main=expression(alpha), xlab="", cex.axis=1,cex.main=3,cex.lab=2)
 polygon(density(alpha_prior), col=rgb(1,0,0,.2))
 polygon(density(alphaout), col=rgb(0,0,1,.2), border=rgb(0,0,1))
 legend("topleft", pch=15, legend =c("prior", "posterior"), 
-       col=c( rgb(1,0,0,.4), rgb(0,0,1,.4)), cex=2)
+       col=c( rgb(1,0,0,.4), rgb(0,0,1,.4)), cex=1.7, bty="n")
 
 beta_prior<-rnorm(nIters,.25,.25);
-plot(density(betaout), xlim=c(-1,1), main="beta", xlab="",ylab="",cex.axis=2,cex.main=3,cex.lab=2)
+plot(density(betaout), xlim=c(-1,1), main=expression(beta), xlab="",ylab="",cex.axis=2,cex.main=3,cex.lab=2)
 polygon(density(beta_prior), col=rgb(1,0,0,.2))
+polygon(density(alpha_prior), col=rgb(1,0,0,.2))
+
 polygon(density(betaout), col=rgb(0,0,1,.2), border=rgb(0,0,1))
 
 sig_o_prior<-abs(rnorm(nIters,.5,.1));
-plot(density(sig_oout), xlim=c(-1,1), main="sig_o",, xlab="", cex.axis=2,cex.main=3,cex.lab=2)
+plot(density(sig_oout), xlim=c(-1,1), main=expression(sigma[o]), xlab="", cex.axis=2,cex.main=3,cex.lab=2)
 polygon(density(sig_o_prior), col=rgb(1,0,0,.2))
 polygon(density(sig_oout), col=rgb(0,0,1,.2), border=rgb(0,0,1))
 
 sig_p_prior<-abs(rnorm(nIters,0,.25));
-plot(density(sig_pout), xlim=c(-1,1), main="sig_p",xlab="",ylab="", cex.axis=2,cex.main=3,cex.lab=2)
+plot(density(sig_pout), xlim=c(-1,1), main=expression(sigma[p]),xlab="",ylab="", cex.axis=2,cex.main=3,cex.lab=2)
 polygon(density(sig_p_prior), col=rgb(1,0,0,.2))
 polygon(density(sig_pout), col=rgb(0,0,1,.2), border=rgb(0,0,1))
+
+
 dev.off()
